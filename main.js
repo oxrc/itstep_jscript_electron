1d7805d26135339346c9f79ab6b931f120f974a3
vberco <vberco.76@gmail.com>
2017-09-28 20:11:17 +0300

    work

--- main.js
+++ main.js
@@ -6,47 +6,58 @@ const path = require('path')
 const url = require('url')
 
 let mainWindow
+let win
 
-function createWindow () {
+function createHowToUsePage() {
+    win = new BrowserWindow({ width: 800, height: 600 })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'how_to_use.html'),
+        protocol: 'file',
+        slashes: true
+    }))
 
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  new_user = new BrowserWindow({width: 1000, height: 800});
-  
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    win.on('closed', function() {
+        win = null
+    })
+}
 
 
-  new_user.loadURL(url.format({
-    pathname: path.join(__dirname, 'add_user.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+function createWindow() {
 
-  
+    mainWindow = new BrowserWindow({ width: 800, height: 600 })
+        //new_user = new BrowserWindow({ width: 1000, height: 800 });
 
- 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+
+    // new_user.loadURL(url.format({
+    //     pathname: path.join(__dirname, 'add_user.html'),
+    //     protocol: 'file:',
+    //     slashes: true
+    // }))
+
+
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
 }
 
 
 app.on('ready', createWindow)
 
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
-
-
