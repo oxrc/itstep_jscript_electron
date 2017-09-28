68d81b98fb1eae1793e202b2395a9cb05b09b6dd
Kyuuby <saruhanov2@mail.ru>
2017-09-28 18:53:50 +0300

    Dopil_Login

--- create_about.js
+++ create_about.js
@@ -1,18 +1,21 @@
-function create_about(){
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+function create_about() {
+  const electron = require('electron');
+  const BrowserWindow = electron.BrowserWindow;
+  const path = require('path')
+  const url = require('url')
+
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'startLogin.html'),
+    pathname: path.join(__dirname, 'about.html'),
     protocol: 'file:',
     slashes: true
   }))
-    mainWindow.on('closed', function () {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
   })
 }
-
-
 exports.create_about = create_about;
--- create_start.js
+++ create_start.js
@@ -1,17 +1,20 @@
-function create_login(){
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+function create_login() {
+  const electron = require('electron');
+  const BrowserWindow = electron.BrowserWindow;
+  const path = require('path')
+  const url = require('url')
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'startLogin.html'),
     protocol: 'file:',
     slashes: true
   }))
-    mainWindow.on('closed', function () {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
   })
 }
-
 exports.create_login = create_login;
