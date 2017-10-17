03c536f2d773e44cbf611d6570e61641c12a522a
MentosMD <ciocoi.cristin@gmail.com>
2017-10-17 19:44:50 +0300

    Added ajax(Add user with service)

--- renderer.js
+++ renderer.js
@@ -1,20 +1,24 @@
 let $ = require('jquery')
 
 $(document).ready(function () {
+
+
+    //Add user
     let name = $('#user_name').val()
     let age = $('#user_age').val()
     let phone = $("#user_phone").val()
-    let interests = $("#user_interests option:selected").text()
+    let interests = $(".user_interests :selected").text()
+    let url = "http://localhost:8000/api/users/add?name"+name+"&age="+age+"&phone="+phone+"&interests="+interests
     $('#btn_send_add').on('click',  (e) => {
         e.preventDefault()
-        $.ajax({
-            url: "http://localhost:8000/api/users/add?name"+name+"&age="+age+"&phone="+phone+"&interests="+interests,
-            method: 'GET',
-            success: (data) => {
-                alert("Success")  
-            }
-        })
-          
+        let xhr = {
+             url: url,
+             method: 'GET',
+             success: (data) => {
+                 alert("Success")  
+             }
+        }
+        $.ajax(xhr)
     })
     
 
